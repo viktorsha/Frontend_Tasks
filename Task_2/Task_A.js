59b19143d89514327e397679b2423b2d94a36c7b
viktorsha <vika.shakun@gmail.com>
2021-07-13 13:25:39 +0300

    Added custom realization of apply function

--- Task_2/Task_A.js
+++ Task_2/Task_A.js
@@ -3,11 +3,27 @@ const context = {x: 2};
 const testThis = function (y) {
     console.log(`x=${this.x}, y=${y}`)
 }
+
+
+Function.prototype.apply = function (context, ...args) {
+    if (typeof this !== 'function') {
+        throw new TypeError('not function')
+    }
+    const func = this
+    context = context || window
+    args = args ? args[0] : []
+    context.func = func
+    let result = context.func(...args)
+    delete context.func
+    return result
+}
+
 const bind = function (func, context) {
     return function (parameter) {
         func.apply(context, [parameter])
     }
 }
+
 testThis(100); // x=1, y=100
 const boundFunction = bind(testThis, context);
 boundFunction(100); // x=2, y=100
