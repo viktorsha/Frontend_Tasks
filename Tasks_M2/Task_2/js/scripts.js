a266566ca57e2af6c1cdfad16d8425257887d9c5
viktorsha <vika.shakun@gmail.com>
2021-07-20 12:19:23 +0300

    Finished 2nd module tasks

---
+++ Tasks_M2/Task_2/js/scripts.js
@@ -0,0 +1,14 @@
+let slideIndex = 1;
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    const slides = document.querySelectorAll(".slider-item");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "flex";
+}
