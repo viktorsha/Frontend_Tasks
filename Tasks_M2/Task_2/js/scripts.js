e70f2f531a493d7dd277e3562b7642ac24c0f16e
viktorsha <vika.shakun@gmail.com>
2021-07-21 10:49:20 +0300

    Changed variable names, code structure, added a favicon, fixed the navigation bar when viewport is less than 600px, fixed slider work

--- Tasks_M2/Task_2/js/scripts.js
+++ Tasks_M2/Task_2/js/scripts.js
@@ -1,14 +1,19 @@
 let slideIndex = 1;
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+
+function plusSlides(slideNumber) {
+    showSlides(slideIndex += slideNumber);
 }
 
-function showSlides(n) {
+function showSlides(slideNumber) {
     const slides = document.querySelectorAll(".slider-item");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    if (slideNumber > slides.length) {
+        slideIndex = 1
+    }
+    if (slideNumber < 1) {
+        slideIndex = slides.length
+    }
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex-1].style.display = "flex";
+    slides[slideIndex - 1].style.display = "flex";
 }
